8f44100497661b32f65761f87b5a18b706387cc5
AngelNJ11 <angelnj733@gmail.com>
2025-06-09 00:33:52 -0500

    Configuración inicial

---
+++ src/main/java/pe/lightbox/LightBoxBeApplication.java
@@ -0,0 +1,13 @@
+package pe.lightbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightBoxBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightBoxBeApplication.class, args);
+    }
+
+}
