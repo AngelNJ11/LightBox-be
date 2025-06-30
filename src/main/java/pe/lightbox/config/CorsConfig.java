5f260b406b3590bf7e45ac6873e9fd0967f4f478
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-06-29 21:29:37 -0500

    refactor: configuracion de CORS para activar authorization en todos los metodos

--- src/main/java/pe/lightbox/config/CorsConfig.java
+++ src/main/java/pe/lightbox/config/CorsConfig.java
@@ -11,11 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
-
-        registry.addMapping("/api/compra/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
